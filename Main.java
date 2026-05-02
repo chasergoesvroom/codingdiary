5e81868f7ecea73a5c54d43bfc45d081a7ff0904
chaser <theoway@protonmail.com>
2026-05-02 14:05:34 +0530

    file name change

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        //please write inside
-        //System.out.print(17);
-        System.out.print(48);
-    }
-    
-}
---
+++ basics_1.java
@@ -0,0 +1,18 @@
+public class basics_1 {
+    public static void main(String[] args) {
+        //please write inside
+        //System.out.print(17);
+        //System.out.print(48);
+        //System.out.print(20+50);
+        //System.out.print(8-5);
+        //System.out.print(4*9);
+
+
+        //System.out.print("10"); //inside quote means as text
+        //System.out.print("Hello world !");
+        //System.out.print(Hello world); --> this cant print cos its not considered as text as theres no quotes
+        //System.out.print("10+5"); even if its number its still considered as text as its in the quotes
+        
+    }
+    
+}
