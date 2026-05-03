47caa98fca9797ba3ad42effa278d7bcd46c266a
chaser <theoway@protonmail.com>
2026-05-03 18:07:23 +0530

    new file

--- intro_to_java_2.java
+++ intro_to_java_2.java
@@ -4,6 +4,38 @@ public class intro_to_java_2 {
     public static void main(String[] args) {
 
         // syntax for variables
+
+        // 1- declaration
+        //<data_type> name_of_the_variable;
+
+        // 2- initialization
+        //<data_type> name_of_the_variable = value;
+
+
+        //initialization
+        int a = 10; 
+        int b = 20;
+        System.out.println(a+b);
+
+        //declaration
+        int c;
+        c = 60;
+        System.out.println(c);
+
+
+
+        int d = 30;
+        int e = 40;
+        System.out.println(d-e);
+
+        //if we initialized a value of variable already can we change it again , yes you can
+        int x = 45;
+        System.out.println(x);
+        x = 26;
+        System.out.println(x);
+
+
+
         
 
 
