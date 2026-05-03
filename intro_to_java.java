c804bba6f2e3cc2bfc3d418e90b2a5f56e1e05a7
chaser <theoway@protonmail.com>
2026-05-03 10:47:26 +0530

    new file

---
+++ intro_to_java.java
@@ -0,0 +1,15 @@
+public class intro_to_java {
+    public static void main(String[] args) {
+        System.out.print("Hello. world!");
+        System.out.print("Hello john");
+        System.out.print("100");
+            
+
+
+
+
+        
+
+    }
+    
+}
